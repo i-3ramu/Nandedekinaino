8ca4e46d1ab109c776f38b7cedbed639ffb732e2
i-3ramu <albahiro@onishi.com>
2020-08-09 18:48:07 +0900

    むかしむかしあるところに…
    そういえば今日電子ノートを買ったんだ
    6万5千円もしたんだよ。でも買って後悔してないんだ。
    だって使ってみたらすごい便利！まるで多目的トイレだね！
    高齢者にも障がい者にもけがした時にも使いやすく、不倫の現場としても使える！
    
    ってそれ多目的トイレの話じゃないか～～～汗汗汗
    
    
    
    
    
    と川に洗濯に行ったおばさんは多目的トイレを見て思ったそうな。

---
+++ src/main/java/com/example/Syukudai/SyukudaiApplication.java
@@ -0,0 +1,9 @@
+package com.example.Syukudai;
+import  org.springframework.boot.SpringApplication;
+import  org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SyukudaiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SyukudaiApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/Syukudai/SyukudaiController.java
@@ -0,0 +1,14 @@
+package com.example.Syukudai;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
+@Controller
+public class SyukudaiController {
+    @RequestMapping("/")
+    public ModelAndView index() {
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("GotoHell");
+        return model;
+    }
+}
