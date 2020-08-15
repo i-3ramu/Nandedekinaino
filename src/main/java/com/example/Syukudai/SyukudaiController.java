158b6c9f4c55243cda93b36caf2fa211ea70f592
i-3ramu <i3ramu6chi@gmail.com>
2020-08-15 13:36:48 +0900

    Take 2

--- src/main/java/com/example/Syukudai/SyukudaiController.java
+++ src/main/java/com/example/Syukudai/SyukudaiController.java
@@ -3,12 +3,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.*;
 @Controller
+
 public class SyukudaiController {
     @RequestMapping("/")
     public ModelAndView index() {
-
-        ModelAndView model = new ModelAndView();
-        model.setViewName("GotoHell");
-        return model;
-    }
+       ModelAndView model = new ModelAndView();
+       model.setViewName("GotoHell");
+       return model;
+   }
 }
